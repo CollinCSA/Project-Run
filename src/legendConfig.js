1074fab3edf2558987bbee44410b9d3329286600
CollinCSA <collin.m@comsolaraz.com>
2025-06-05 11:05:12 -0700

    Add files via upload

---
+++ src/legendConfig.js
@@ -0,0 +1,84 @@
+export const legendConfig = {
+  "fields": [
+    [
+      "Inverter Type",
+      [
+        "30W",
+        "30W"
+      ]
+    ],
+    [
+      "Inverter Quantity",
+      [
+        1,
+        2
+      ]
+    ],
+    [
+      "Power Stub Up Size 1",
+      [
+        "1\"",
+        "1-1/4\""
+      ]
+    ],
+    [
+      "Power Stub Up Size 2",
+      []
+    ],
+    [
+      "Combiner?",
+      [
+        "No",
+        "Yes"
+      ]
+    ],
+    [
+      "Disco/Comb to Inverter 1 Size",
+      [
+        "1\"",
+        "1\""
+      ]
+    ],
+    [
+      "Combiner to Inverter 2 Size",
+      [
+        "1\""
+      ]
+    ],
+    [
+      "Combiner to UG",
+      []
+    ],
+    [
+      "Purlin Quantity",
+      [
+        7,
+        7
+      ]
+    ]
+  ],
+  "valid_combinations": {
+    "type1": [
+      "30W",
+      1,
+      "1\"",
+      NaN,
+      "No",
+      "1\"",
+      NaN,
+      NaN,
+      7
+    ],
+    "type2": [
+      "30W",
+      2,
+      "1-1/4\"",
+      NaN,
+      "Yes",
+      "1\"",
+      "1\"",
+      NaN,
+      7
+    ]
+  }
+};
